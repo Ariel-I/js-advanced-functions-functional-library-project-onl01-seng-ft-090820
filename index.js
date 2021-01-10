b13d0fff7910320623cde1045d0a66826927551f
Ariel_Ibarra <arielibarra39@gmail.com>
2021-01-10 13:17:25 -0800

    commit tree 62d131529e1a7014d7a9065d3e3be9c1aa2c9567

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let array = Object.values(collection)
+      for (let i = 0; i < array.length; i++) {
+        callback(array[i], i, array)
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let array = Object.values(collection)
+      let newArray = []
+      for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array))
+      }
+      return newArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      let array = Object.values(collection)
+      for (let i = 0; i < array.length; i++) {
+        callback(array[i], i, array)
+      }
+      return collection
     },
 
     functions: function() {
